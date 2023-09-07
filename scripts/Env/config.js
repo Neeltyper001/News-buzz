df3b56fd1f367146e2dd1f55bb94d1b96dbfdfae
Neelesh Joshi <neeleshjoshi001@gmail.com>
2023-09-07 09:39:59 +0530

    added news-model.js updated api-client.js

---
+++ scripts/Env/config.js
@@ -0,0 +1,2 @@
+const Url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=b9696d7845164da6a06de3b1f4af46a8'
+export default Url;
--- scripts/Services/api-client.js
+++ scripts/Services/api-client.js
@@ -1,1 +1,15 @@
-console.log("Api-client running...")
+import Url from "../Env/config.js";
+
+const getData = async (url) =>{
+    try {
+        const response = await fetch(url);
+        console.log(response)
+        const data  = await response.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log("Error while fetching data from API", error)
+    }
+}
+
+getData(Url)
