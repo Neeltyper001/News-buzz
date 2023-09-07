a10d3fa70c2c943715ea58a0f38384eca534c361
Neelesh Joshi <neeleshjoshi001@gmail.com>
2023-09-07 08:29:41 +0530

    added dist.output.css package-lock.json package.json tailwind.config.js index.html scripts(directory)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+const moduleExports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default moduleExports
+
