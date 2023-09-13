a726892c01a3d9693804f37f2064775d6cf9f21a
Neelesh Joshi <neeleshjoshi001@gmail.com>
2023-09-13 23:20:58 +0530

    id = updated dotenv.config(); const Url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}` export default Url;

--- scripts/Env/config.js
+++ scripts/Env/config.js
@@ -1,3 +1,4 @@
-const Url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey=b9696d7845164da6a06de3b1f4af46a8'
-// const Url = 'https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=in&max=10&apikey=49f043047c6a0a00b6150d6c005723fa'
+import dotenv from 'dotenv';
+dotenv.config();
+const Url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.API_KEY}`
 export default Url;
