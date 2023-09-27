d8d8f65b6f13d49d40bccc40c90241379052f714
Neelesh Joshi <neeleshjoshi001@gmail.com>
2023-09-27 22:33:32 +0530

    Updated url

--- scripts/Env/config.js
+++ scripts/Env/config.js
@@ -1,3 +1,3 @@
-// const Url = `https://news-buzz-back-end.onrender.com/api/getData/`
-const Url = `http://localhost:3000/api/getData/`
+const Url = `https://news-buzz-back-end.onrender.com/api/getData/`
+// const Url = `http://localhost:3000/api/getData/`
 export default Url;
